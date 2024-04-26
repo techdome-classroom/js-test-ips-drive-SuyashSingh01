42df1cbfb160990e8153490ec7b6ed914e9b1a27
Suyash singh <singhsuyash045@gmail.com>
2024-04-26 13:34:58 +0530

    problem solved

--- program1.js
+++ program1.js
@@ -1,9 +1,15 @@
 function smallestMissingPositiveInteger(nums) {
-
-
-    // Implement the function smallest_missing_positive_integer
-  
+  nums.sort((a, b)=>a-b);
+  let ans = 1;
+  for(let i = 0; i < nums.length; i++)
+  {
+      if(nums[i] == ans)
+          ans++;
   }
+  return ans;
   
+
+}
+
   module.exports = smallestMissingPositiveInteger;
 
--- program2.js
+++ program2.js
@@ -1,7 +1,43 @@
 function longestSubstring(s) {
+    // if string length is 0
+    if (s.length === 0)
+        return 0;
 
+    // if string length 1
+    if (s.length === 1)
+        return 1;
 
-    // Implementation of longestSubstring function
+    // if sing length is more than 2
+    let maxLength = 0;
+    let visited = new Array(256).fill(false);
+
+    // left and right pointer of sliding window
+    let left = 0, right = 0;
+    while (right < s.length) {
+
+        // if character is visited
+        if (visited[s.charCodeAt(right)]) {
+
+            // The left pointer moves to the right while
+            // marking visited characters as false until the
+            // repeating character is no longer part of the
+            // current window.
+            while (visited[s.charCodeAt(right)]) {
+                visited[s.charCodeAt(left)] = false;
+                left++;
+            }
+        }
+
+        visited[s.charCodeAt(right)] = true;
+
+        // The length of the current window (right - left + 1)
+        // is calculated and the answer is updated accordingly.
+        maxLength = Math.max(maxLength, right - left + 1);
+        right++;
+    }
+
+    return maxLength;
 }
+
 
 module.exports = { longestSubstring };
